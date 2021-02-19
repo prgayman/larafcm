aaa4a4be51834b0e8dc07e1ca93490640b817454
Ayman Alaiwah <aymanalaiwah.dev@gmail.com>
2021-02-19 10:20:01 +0200

    fix message

--- src/Exceptions/LaraFcmException.php
+++ src/Exceptions/LaraFcmException.php
@@ -8,11 +8,11 @@ class LaraFcmException extends Exception
 {
     public static function inValidNotification()
     {
-        return new static("Notification object not valid, to send notifiaction must be set notification object from  [Prgayman\LaraFcm\Message\Notification]");
+        return new static("Notification is not valid, to send notifiaction must be set notification object from  [Prgayman\LaraFcm\Message\Notification]");
     }
 
     public static function inValidData()
     {
-        return new static("Message object not valid, to send message must be set data object from  [Prgayman\LaraFcm\Message\Data]");
+        return new static("Message is not valid, to send message must be set data object from [Prgayman\LaraFcm\Message\Data]");
     }
 }
